0cdea3c9be6b26073f6dabf682cca8e03c608154
Anatsrisaputh <you@example.com>
2020-02-20 22:44:27 +0700

    Learn Java sript

---
+++ homeworkrjanson/javascript.js
@@ -0,0 +1,36 @@
+// alert( null || 2 || undefined ); //2
+
+//alert( alert(1) || 2 || alert(3) ); // 1,2
+
+//alert( 1 && null && 2); // null
+
+//alert( alert(1) && alert(2) ); // 1,undefined
+
+//alert( null || 2 && 3 || 4 ); // 3
+
+// for (let i = 0; < 3; i++) {
+
+// }
+// let sum = 0;
+// for (let i = 1; i <= 1027; i = i + 2) {
+//     sum += i;
+// }
+// console.log(sum);
+
+
+// ข้อ 1.9.5
+// if (userName === "Admin") {
+//     let password = prompt ("Enter your password")
+//         if (password === "codecamp#5") {
+//             alert("ยินดีต้อนรับ");
+//         } else if (password === "" || password === null) {
+//             alert("ยกเลิก");
+//         } else {
+//             alert("รหัสผ่านผิด");
+//         }
+// } else if (userName === "" || userName === null) {
+//     alert("ยกเลิก");
+// } else {
+//     alert("ฉันไม่รู้จักคุณ");
+// }
+
